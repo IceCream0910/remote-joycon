a12e411a09b687c1aa1352faf6a69914640e3f5b
icecream0910 <taein2370@naver.com>
2023-11-20 00:31:57 +0900

    UPDATE: GUI improve

--- run.py
+++ run.py
@@ -1,54 +1,87 @@
-import tkinter as tk
+import sys
+import socket
 import subprocess
-from threading import Thread
-import tkinter.messagebox as messagebox
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QVBoxLayout, QMessageBox
+from PyQt5.QtGui import QPixmap
+import qrcode
 
-class CommandExecutor:
+class ServerControlApp(QWidget):
     def __init__(self):
-        self.root = tk.Tk()
-        self.root.title("Joy-con")
-        self.root.geometry("500x150")  # Adjusted the size for better display
+        super().__init__()
+        self.client_process = None
+        self.server_process = None
+        self.initUI()
 
-        # 첫 번째 버튼 (왼쪽 절반)
-        self.client_button = tk.Button(self.root, text="서버 시작하기", command=self.run_client, bg="#ff5e52", fg="white")
-        self.client_button.grid(row=0, column=0, padx=5, pady=10, sticky="nsew")
+    def initUI(self):
+        self.setWindowTitle('가상 조이콘 연결')
+        self.setGeometry(100, 100, 500, 200)
 
-        # Text 위젯 생성 (ipconfig 및 콘솔 출력용)
-        self.output_text = tk.Text(self.root, wrap="word", height=10, width=40)
-        self.output_text.grid(row=1, column=0, columnspan=2, padx=10, pady=10, sticky="nsew")
+        # 서버 시작 버튼 생성 및 클릭 이벤트 연결
+        self.startButton = QPushButton('통신 서버 시작', self)
+        self.startButton.setStyleSheet("background-color: #02bbde; padding: 20px; color: white; border-radius: 10px;")
+        self.startButton.clicked.connect(self.startServer)
 
-        # 행과 열 가중치를 설정하여 창 크기 조절 시 위젯 크기를 조절
-        self.root.grid_rowconfigure(1, weight=1)
-        self.root.grid_columnconfigure(0, weight=1)
-        self.root.grid_columnconfigure(1, weight=1)
+        # 프로세스 및 GUI 종료 버튼 생성 및 클릭 이벤트 연결
+        self.exitButton = QPushButton('종료', self)
+        self.exitButton.setStyleSheet("background-color: #ff5e52; padding: 20px; color: white; border-radius: 10px;")
+        self.exitButton.clicked.connect(self.exitApp)
 
+        # 이미지 뷰어 라벨 생성
+        self.qrLabel = QLabel(self)
 
-    def run_client(self):
-        self.run_command("python client.py")
-        self.run_command("cd web && python -m http.server 3000")
-        messagebox.showinfo("서버 시작", "서버가 성공적으로 시작되었습니다.")
+        self.helpLabel = QLabel(self)
 
-    def run_command(self, command):
-        def run():
-            try:
-                result = subprocess.run(command, shell=True, check=True, capture_output=True, text=True)
-                output = result.stdout.strip()
-                self.show_output(output)
-            except subprocess.CalledProcessError as e:
-                error_output = e.stderr.decode('utf-8').strip()
-                self.show_output(f"Error:\n{error_output}")
+        # 레이아웃 설정
+        layout = QVBoxLayout()
+        layout.addWidget(self.startButton)
+        layout.addWidget(self.exitButton)
+        layout.addWidget(self.qrLabel)
+        layout.addWidget(self.helpLabel)
+        self.setLayout(layout)
 
-        # 새 스레드에서 명령 실행
-        Thread(target=run).start()
+        self.show()
 
-    def show_output(self, output):
-        # 기존 텍스트 삭제 후 결과 추가
-        self.output_text.delete(1.0, tk.END)
-        self.output_text.insert(tk.END, output)
+    def startServer(self):
+        # 명령어 실행 및 프로세스 저장
+        self.client_process = subprocess.Popen(["python", "client.py"], shell=True)
+        web_directory = "C:\Users\icecr/Desktop/joycon/web" # 실행하는 PC에 맞게 경로 수정
+        self.server_process = subprocess.Popen(["python", "-m", "http.server", "3000"], shell=True, cwd=web_directory)
+        QMessageBox.information(self, "서버 시작", "서버가 시작되었습니다.")
 
-    def run(self):
-        self.root.mainloop()
+        # 현재 PC의 IP 주소 얻기
+        ip_address = socket.gethostbyname(socket.gethostname())
 
-if __name__ == "__main__":
-    executor = CommandExecutor()
-    executor.run()
+        # QR 코드 생성 및 표시
+        qr = qrcode.QRCode(
+            version=1,
+            error_correction=qrcode.constants.ERROR_CORRECT_L,
+            box_size=15,
+            border=4
+        )
+        qr.add_data(f'http://{ip_address}:3000/v2')
+        qr.make(fit=True)
+        img = qr.make_image(fill_color="black", back_color="white")
+        img.save("qr_code.png")
+
+        # 이미지 뷰어에 QR 코드 표시
+        pixmap = QPixmap("qr_code.png")
+        self.qrLabel.setPixmap(pixmap)
+        self.qrLabel.resize(pixmap.width(), pixmap.height())
+        self.helpLabel.setText("      스마트폰으로 QR 코드를 스캔하세요")
+
+    def exitApp(self):
+        # 실행 중인 서버 프로세스 종료
+        if self.client_process:
+            self.client_process.terminate()
+            self.client_process.wait()
+        if self.server_process:
+            self.server_process.terminate()
+            self.server_process.wait()
+
+        # GUI 종료
+        self.close()
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = ServerControlApp()
+    sys.exit(app.exec_())
